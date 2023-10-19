85d1030de0a7a48f36bef63a089861e2757334fd
Ephaste <ephasteniyonizeye@gmail.com>
2023-10-19 10:38:41 +0200

    modiication

--- src/routes/booking.js
+++ src/routes/booking.js
@@ -1,25 +1,15 @@
 
 import express  from "express";
-import multer from "multer";
-
-import { verifyToken,logger, isAdmin } from "../middleware";
-const  bookingsRouter = express.Router();
-const storage = multer.diskStorage({
-    destination: function(req,file,cb){
-        console.log(req);
-        cb(null, "user_assets");
-    },
-    filename: function(req, file,cb){
-        cb(null, file.originalname);
-    },
-});
-const upload = multer({storage,});
+import { isAdmin } from "../middleware";
+const bookingsRouter = express.Router();
 
 import {getAllb,addnewb, getbyIdb} from "../controllers/crud";
 
+
+
 //usersRouter.use(verifyToken);
 bookingsRouter.get("/", getAllb);
-bookingsRouter.post("/",upload.single("image"),logger, addnewb);
+bookingsRouter.post("/", addnewb);
 // studentsRouter.delete("/:id",removeData);
 // studentsRouter.put("/:id",putData);
  bookingsRouter.get("/:id",isAdmin, getbyIdb);
